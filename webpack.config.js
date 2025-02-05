0e6a03b9a3d3ce488ea84dae35f2f6d772c4d156
Daria <ruksova17@gmail.com>
2025-02-05 11:53:06 +0400

    Настраивает хэширование

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,32 @@
 const path = require('node:path');
 const CopyPlugin = require('copy-webpack-plugin');
+const HtmlPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/main.js', // точка входа
   output: {
-    filename: 'bundle.js', // имя бандла
+    filename: 'bundle.[contenthash].js', // имя бандла
     path: path.resolve(__dirname, 'build'), // директория для файлов сборки
-    clean: true, // удаляем предыдущую сборку перед созданием новой
+    clean: true, // удаление предыдущей сборки перед созданием новой
   },
-  devtool: 'source-map', // генерируем карту исходного кода
-  plugins: [ // Подключаем плагины
+  devtool: 'source-map', // генерирование карты исходного кода
+  plugins: [ // подключение плагинов
+    new HtmlPlugin({
+      template: 'public/index.html',
+    }),
     new CopyPlugin({
-      patterns: [{ from: 'public' }],
+      patterns: [
+        {
+          from: 'public',
+          globOptions: {
+            ignore: ['**/index.html'],
+          },
+        },
+      ],
     }),
   ],
   module: {
-    rules: [
+    rules: [ // добавление лоадеров
       {
         test: /\.js$/,
         exclude: /(node_modules)/,
