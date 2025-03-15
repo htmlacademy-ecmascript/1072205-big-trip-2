de2ea756d9839847a7e2788870acd2e5c691035f
Daria <ruksova17@gmail.com>
2025-03-15 21:58:57 +0400

    Реализует логику работы сортировки

--- src/presenter/events-list-presenter.js
+++ src/presenter/events-list-presenter.js
@@ -1,10 +1,7 @@
 import { render } from '../framework/render.js';
-import { generateFilter } from '../utils/filter.js';
-import { generateSort } from '../utils/sort.js';
-import { updateItem } from '../utils/event.js';
 import EventListView from '../view/event-list-view.js';
-import FiltersView from '../view/filters-view.js';
 import SortView from '../view/sort-view.js';
+import { generateSort } from '../utils/sort.js';
 import EventPresenter from './event-presenter.js';
 
 export default class EventsListPresenter {
@@ -13,11 +10,9 @@ export default class EventsListPresenter {
   #destinations = [];
   #offers = [];
   #eventPresenters = new Map();
+  #tripEventElement = null;
 
   #eventListComponent = new EventListView();
-
-  #tripEventElement = null;
-  #filtersElement = null;
 
   constructor(eventsModel) {
     this.#eventsModel = eventsModel;
@@ -29,19 +24,19 @@ export default class EventsListPresenter {
     this.#offers = [...this.#eventsModel.offers];
 
     this.#tripEventElement = document.querySelector('.page-main .trip-events');
-    this.#filtersElement = document.querySelector('.trip-controls__filters');
 
-    this.#renderFilters();
     this.#renderSort();
     this.#renderEventList();
   }
 
-  #renderFilters() {
-    render(new FiltersView({ filters: generateFilter(this.#events) }), this.#filtersElement);
-  }
-
   #renderSort() {
-    render(new SortView({ sorts: generateSort() }), this.#tripEventElement);
+    const sorts = generateSort();
+    const sortView = new SortView({
+      sorts,
+      onSortChange: this.#handleSortChange,
+    });
+    render(sortView, this.#tripEventElement);
+    sortView.setEventListeners();
   }
 
   #renderEventList() {
@@ -51,24 +46,37 @@ export default class EventsListPresenter {
         this.#tripEventElement.querySelector('.trip-events__list')
       );
 
-      eventPresenter.init(event, this.#destinations, this.#offers, this.#onEventChange, this.#resetEventViews);
+      eventPresenter.init(event, this.#destinations, this.#offers);
       this.#eventPresenters.set(event.id, eventPresenter);
     });
   }
 
-  // Метод для обновления события
-  #onEventChange = (updatedEvent) => {
-    this.#events = updateItem(this.#events, updatedEvent);
-    this.#eventPresenters.get(updatedEvent.id)?.update(updatedEvent);
-  };
+  #handleSortChange = (sortType) => {
+    // Сортировка в зависимости от типа
+    switch (sortType) {
+      case 'day':
+        this.#events.sort((a, b) => new Date(a.dateFrom) - new Date(b.dateFrom));
+        break;
+      case 'event':
+        this.#events.sort((a, b) => a.destination.localeCompare(b.destination));
+        break;
+      case 'time':
+        this.#events.sort((a, b) => new Date(a.dateFrom) - new Date(b.dateFrom));
+        break;
+      case 'price':
+        this.#events.sort((a, b) => a.price - b.price);
+        break;
+      case 'offers':
+        this.#events.sort((a, b) => a.offers.length - b.offers.length);
+        break;
+      default:
+        break;
+    }
 
-  // Метод для закрытия всех открытых форм
-  #resetEventViews = () => {
-    this.#eventPresenters.forEach((presenter) => presenter.resetView());
-  };
+    // Очищаем текущий список
+    this.#eventListComponent.clear();
 
-  getEventPresenters() {
-    return this.#eventPresenters;
+    // Перерисовываем список с отсортированными событиями
+    this.#renderEventList();
   }
 }
-
--- src/view/sort-view.js
+++ src/view/sort-view.js
@@ -1,21 +1,28 @@
 import AbstractView from '../framework/view/abstract-view';
 
-function createSortItemTemplate(sort) {
-  const {type, isChecked} = sort;
+function createSortItemTemplate(sort, onSortChange) {
+  const { type, isChecked } = sort;
 
   return (
-    `<div class="trip-sort__item  trip-sort__item--${type}">
-      <input id="sort-${type}" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-${type}" ${isChecked && 'checked'}>
-      <label class="trip-sort__btn" for="sort-${type}">${type}</label>
+    `<div class="trip-sort__item trip-sort__item--${type}">
+      <input
+        id="sort-${type}"
+        class="trip-sort__input visually-hidden"
+        type="radio"
+        name="trip-sort"
+        value="sort-${type}"
+        ${isChecked ? 'checked' : ''}
+        data-sort-type="${type}">
+      <label class="trip-sort__btn" for="sort-${type}" data-sort-type="${type}">${type}</label>
     </div>`
   );
 }
 
-function createSortTemplate(sortItems) {
-  const sortItemsTemplate = sortItems.map((sort, index) => createSortItemTemplate(sort, index === 0)).join('');
+function createSortTemplate(sortItems, onSortChange) {
+  const sortItemsTemplate = sortItems.map((sort) => createSortItemTemplate(sort, onSortChange)).join('');
 
   return (
-    `<form class="trip-events__trip-sort  trip-sort" action="#" method="get">
+    `<form class="trip-events__trip-sort trip-sort" action="#" method="get">
       ${sortItemsTemplate}
     </form>`
   );
@@ -23,13 +30,24 @@ function createSortTemplate(sortItems) {
 
 export default class SortView extends AbstractView {
   #sorts = null;
+  #onSortChange = null;
 
-  constructor({sorts}) {
+  constructor({ sorts, onSortChange }) {
     super();
     this.#sorts = sorts;
+    this.#onSortChange = onSortChange;
   }
 
   get template() {
-    return createSortTemplate(this.#sorts);
+    return createSortTemplate(this.#sorts, this.#onSortChange);
+  }
+
+  setEventListeners() {
+    this.element.querySelectorAll('.trip-sort__input').forEach((input) => {
+      input.addEventListener('change', (evt) => {
+        const sortType = evt.target.dataset.sortType;
+        this.#onSortChange(sortType); // вызываем переданный callback
+      });
+    });
   }
 }
