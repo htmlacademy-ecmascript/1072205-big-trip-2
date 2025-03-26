d6085051c0622be073841597138b3396e22fcc2b
Daria <ruksova17@gmail.com>
2025-03-26 22:02:06 +0400

    Дорабатывает расчет общей стоимости путешествия

--- src/view/trip-info-view.js
+++ src/view/trip-info-view.js
@@ -3,16 +3,42 @@ import { humanizeDate } from '../utils/event';
 
 const EVENT_DATE_FORMAT = 'DD MMM';
 
-function createTripInfoTemplate(events, destinations) {
+function createTripInfoTemplate(events, destinations, offers) {
   if (!events.length) {
     return '';
   }
 
   const destinationNames = events.map((event) => destinations.find((dest) => dest.id === event.destination).name);
   const tripInfoTitle = destinationNames.join(' &mdash; ');
+
   const tripStartTime = Math.min(...events.map((event) => event.dateFrom));
   const tripEndTime = Math.max(...events.map((event) => event.dateTo));
-  const totalCost = events.reduce((sum, event) => sum + event.basePrice, 0);
+
+  console.log('Events:', events);
+console.log('Offers:', offers);
+events.forEach((event) => {
+  console.log(`Event: ${event.type}, Base price: ${event.basePrice}, Offers:`, event.offers);
+});
+const totalCost = events.reduce((sum, event) => {
+  let eventCost = event.basePrice;
+
+  console.log(`Processing event: ${event.type}, Base price: ${event.basePrice}`);
+
+  if (Array.isArray(event.offers) && event.offers.length > 0) {
+    // Сразу прибавляем стоимость всех офферов события
+    const selectedOffersCost = event.offers.reduce((offerSum, offer) => {
+      console.log(`Adding offer price: ${offer.price} for offer: ${offer.title}`);
+      return offerSum + offer.price;
+    }, 0);
+
+    eventCost += selectedOffersCost;
+  }
+
+  console.log(`Final cost for event ${event.type}: ${eventCost}`);
+  return sum + eventCost;
+}, 0);
+
+
 
   return (
     `<section class="trip-main__trip-info  trip-info">
@@ -30,17 +56,20 @@ function createTripInfoTemplate(events, destinations) {
   );
 }
 
+
 export default class TripInfoView extends AbstractView {
   #events;
   #destinations;
+  #offers;
 
-  constructor({ events, destinations }) {
+  constructor({ events, destinations, offers }) {
     super();
     this.#events = events;
     this.#destinations = destinations;
+    this.#offers = offers;
   }
 
   get template() {
-    return createTripInfoTemplate(this.#events, this.#destinations);
+    return createTripInfoTemplate(this.#events, this.#destinations, this.#offers);
   }
 }
